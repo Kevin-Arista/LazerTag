ead67e67d7abb490f5b6f55ad518759066d566ca
kevin <kevin,a8988@gmail.com>
2023-04-12 16:23:01 -0400

    created form, with styling modifications

--- client-frontend/src/pages/about.js
+++ client-frontend/src/pages/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MemberCard from '../components/member-card';
 import './about.css';
 import Kevin from '../assets/Kevin.jpg';
+import Evelio from '../assets/Evelio.jpeg';
 import Ezzadin from '../assets/Ezzadin.jpeg';
 import Sophya from '../assets/Sophya.jpg';
 import Rosnel from '../assets/Rosnel.jpg'
@@ -13,21 +14,18 @@ function About(props){
             <h1>Meet the Team!</h1>
             
             <div className='team-section'>
-            <h2>Electrical Team</h2>
-            <MemberCard name="Hannah" description="- worked on circuit design and schematics of the blaster model" major="Intended Major: Electrical Engineering" png={Hannah}/>
-            <MemberCard name="Rosnel Leyva" description="- worked on circuit design and schematics of the blaster model"  major="Intended: Electrical Engineering B.S." png={Rosnel}/>
+            <MemberCard name="Hannah" description="worked on circuit design and schematics of the blaster model" major="Intended Major: Electrical Engineering" png={Hannah}/>
+            <MemberCard name="Rosnel Leyva" description="worked on circuit design and schematics of the blaster model"  major="Intended: Electrical Engineering B.S." png={Rosnel}/>
             </div>
             
             <div className='team-section'>
-            <h2>Mechanical Team</h2>
-            <MemberCard name="Sophya Elkihel" description="- researched and sampled different architecture for the blaster design" major="Intended Major: Mechanical Engineering" png={Sophya}/>
-            <MemberCard name="Ezzadin Aldaylam" description="- designed and developed the blaster's design into production" major="Intended Major: Mechanical Engineering" png={Ezzadin}/>
+            <MemberCard name="Sophya Elkihel" description="researched and sampled different architecture for the blaster design" major="Intended Major: Mechanical Engineering" png={Sophya}/>
+            <MemberCard name="Ezzadin Aldaylam" description="designed and developed the blaster's design into production" major="Intended Major: Mechanical Engineering" png={Ezzadin}/>
             </div>
             
             <div className='team-section'>
-            <h2>Software Team</h2>
-            <MemberCard name="Evelio Velez" description="- designed, tested, and developed software for the blasters" major="Intended Major: Computer Science" png={Rosnel}/>
-            <MemberCard name="Kevin Arista" description="- designed UX/UI, developed application for LazzarX using MERNstack" major="Intended Major: Computer Science" png={Kevin}/>
+            <MemberCard name="Evelio Velez" description="designed, tested, and developed software for the blasters" major="Intended Major: Computer Science" png={Evelio}/>
+            <MemberCard name="Kevin Arista" description="designed UX/UI, developed application for LazzarX using MERNstack" major="Intended Major: Computer Science" png={Kevin}/>
             </div>
         </div>
     );
--- client-frontend/src/pages/reservations.js
+++ client-frontend/src/pages/reservations.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
+import './reservations.css';
 
 function Reservations(props){
-    return(
-        <div className="Reservations">
-            This is the reservations page!
-        </div>
-    );
+    
+    const [formData, setFormData] = useState({
+        fname: "",
+        lname: "",
+        partysize: "",
+        countrycode: "",
+        phonenumber: ""
+     })
+     
+     const handleSubmit = (e)=>{
+         e.preventDefault()
+         axios.post('//localhost:4000/appointments/post', formData).then((res) => {
+             console.log(res.status, res.data);
+         });
+         setFormData({
+            fname: "",
+            lname: "",
+            partysize: "",
+            countrycode: "",
+            phonenumber: ""
+         });
+         alert("Your reservation has been made!");
+     }
+ 
+     return(
+         
+         <div className="Reservations">
+             
+             <form onSubmit={handleSubmit}>
+                 <h3 >First Name</h3>
+                 <input onChange={(e)=> setFormData({...formData, fname: e.target.value})} value={formData.fname} type="text" name="fname" id="fname" />
+                 <br/>
+                 <h3>Last Name</h3>
+                 <input onChange={(e)=> setFormData({...formData, lname: e.target.value})} value={formData.lname} type="text" name="lname" id="lname" />
+                 <br/>
+                 <h3>Party Size</h3>
+                 <input onChange={(e)=> setFormData({...formData, partysize: e.target.value})} value={formData.partysize} type="text" name="partysize" id="partysize" />
+                 <br/>
+                 <h3>Country Code</h3>
+                 <input onChange={(e)=> setFormData({...formData, countrycode: e.target.value})} value={formData.countrycode} type="text" name="countrycode" id="countrycode" />
+                 <br/>
+                 <h3>Phone Number</h3>
+                 <input onChange={(e)=> setFormData({...formData, phonenumber: e.target.value})} value={formData.phonenumber} type="text" name="phonenumber" id="phonenumber" />
+                 <br/>
+                 <input id="button" type="submit" value="Submit" />
+             </form>    
+         </div>
+     );
 }
 
 export default Reservations;
