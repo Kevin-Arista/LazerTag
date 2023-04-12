c06891ac79d2f85041d68d758d932612d1531ae6
kevin <kevin,a8988@gmail.com>
2023-04-12 18:29:11 -0400

    fixed maxlength of entries

--- backend/models/appointment.model.js
+++ backend/models/appointment.model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const AppointmentSchema = new mongoose.Schema({
     fname: {type: String, require: true, maxLength:70, default: "unknown"},
     lname: {type: String, require: true, maxLength:70, default: "unknown"},
-    partysize: {type: String, require: true, maxlength: 2, default: "1"},
-    countrycode: {type: String, require: true, maxlength: 5, default: "1"},
-    phonenumber: {type: String, require: true, maxlength: 15, default: "0000000000"}
+    partysize: {type: String, require: true, maxlength: 10, default: "1"},
+    countrycode: {type: String, require: true, maxlength: 10, default: "1"},
+    phonenumber: {type: String, require: true, maxlength: 20, default: "0000000000"}
 });
 
 const AppointmentModel = mongoose.model("AppointmentModel", AppointmentSchema);
